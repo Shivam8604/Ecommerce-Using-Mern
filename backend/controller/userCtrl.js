e606813282cf4aee120d5c32b69bd8a1b53d36ec
Shivam pal <devshivampal360@gamil.com>
2024-10-17 16:56:27 +0530

    crete user auth

--- backend/controller/userCtrl.js
+++ backend/controller/userCtrl.js
@@ -80,6 +80,16 @@ const userCtrl = {
         }catch(error){
             
         }
+    },
+
+    getUser:async(req,res)=>{
+        try{
+            const user = await Users.findById(req.user.id).select('-password');
+            if(!user) return res.status(400).json({msg:"User not found"})
+            res.json(user)
+        }catch(err){
+
+        }
     }
 
 }
--- backend/routes/useRouter.js
+++ backend/routes/useRouter.js
@@ -6,5 +6,6 @@ router.post('/register',userCtrl.register)
 router.post('/login',userCtrl.login)
 router.get('/logout',userCtrl.logout)
 router.post('/refreshtoken',userCtrl.refreshtoken);
+router.get("/infor",auth,)
 
 module.exports = router
